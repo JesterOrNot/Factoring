a655ba9af2857c265c60f5b4357f7eb132b33be5
Sean Hellum <seanhellum45@gmail.com>
2019-11-05 03:23:23 +0000

    format

--- app.java
+++ app.java
@@ -7,34 +7,35 @@ import java.util.HashMap;
 public class app {
     static ArrayList<Integer> findFactors(int num) {
         ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = num; i>=1; i--) {
+        for (int i = num; i >= 1; i--) {
             if (num % i == 0) {
                 list.add(i);
             }
         }
         return list;
     }
-    static HashMap<Integer,Integer> assembleTable(ArrayList<Integer> list) {
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+    static HashMap<Integer, Integer> assembleTable(ArrayList<Integer> list) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         int length = 0;
         for (int var : list) {
             length++;
         }
         int numOfPairs = length / 2;
-        for(int i = 1; i<=numOfPairs; i++) {
+        for (int i = 1; i <= numOfPairs; i++) {
             int item1 = list.get(list.size() - 1);
             int item2 = list.get(0);
-            map.put(item1,item2);
+            map.put(item1, item2);
             list.remove(0);
-            list.remove(length-2);
-            length-=2;
+            list.remove(length - 2);
+            length -= 2;
         }
         return map;
     }
 
     public static void main(String[] args) {
         ArrayList<Integer> list = findFactors(66);
-        HashMap<Integer,Integer> map = assembleTable(list);
+        HashMap<Integer, Integer> map = assembleTable(list);
         for (Object i : map.values()) {
             System.out.print(i + " ");
         }
