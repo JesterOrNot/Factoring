11667d3618fa93dab3145cfc30c8c0d5ed306faa
Sean Hellum <seanhellum45@gmail.com>
2019-11-05 15:47:34 +0000

    update

--- app.java
+++ app.java
@@ -32,27 +32,33 @@ public class app {
         }
         return map;
     }
-    public static int[] findRightFactorSet(ArrayList<Integer> keys,ArrayList<Integer> values,int bVal, int Cval) {
+    public static ArrayList<Integer> findRightFactorSet(ArrayList<Integer> keys,ArrayList<Integer> values,int bVal, int Cval) {
+        ArrayList<Integer> awns = new ArrayList<Integer>();
         int length = 0;
-        int[] bsArray = {0,0};
         for (int var : keys) {
             length++;
         }
         int numOfPairs = length / 2;
-        for (int i = 1; i<=numOfPairs; i++) {
+        for (int i = 0; i<numOfPairs; i++) {
             if ((keys.get(i) + values.get(i) == bVal) && (keys.get(i) * values.get(i) == Cval)) {
-                bsArray[0] = keys.get(i);
-                bsArray[1] = values.get(i);
+                awns.add(keys.get(i));
+                awns.add(values.get(i));
             }
         }
-        return bsArray;
+        return awns;
     }
     public static void main(String[] args) {
-        ArrayList<Integer> list = findFactors(-70);
+        ArrayList<Integer> list = findFactors(6);
         HashMap<Integer, Integer> map = assembleTable(list);
         ArrayList<Integer> keys = new ArrayList<Integer>();
         ArrayList<Integer> values = new ArrayList<Integer>();
-        int[] myArray = findRightFactorSet(keys, values, -3, -70);
-        System.out.println("Factor 1 is " + myArray[0] + " and " + myArray[1]);
+        for (int i : map.keySet()) {
+            keys.add(i);
+        }
+        for (int i : map.values()) {
+            values.add(i);
+        }
+        System.out.println(findRightFactorSet(keys, values, 7, 6));
+        System.out.println(keys + "\n" + values);
     }
 }
