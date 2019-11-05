a79142473e7b1dc8ca0b71f4031c72d5ba774d56
Sean Hellum <seanhellum45@gmail.com>
2019-11-05 00:09:42 +0000

    update app

--- app.java
+++ app.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * app
@@ -12,6 +13,23 @@ public class app {
             }
         }
         return list;
+    }
+    static HashMap assembleTable(ArrayList<Integer> list) {
+        HashMap map = new HashMap<>();
+        int length = 0;
+        for (Object var : list) {
+            length++;
+        }
+        int numOfPairs = length / 2;
+        for(int i = 1; i<=numOfPairs; i++) {
+            int item1 = list.get(length - 1);
+            int item2 = list.get(0);
+            map.put(item1,item2);
+            list.remove(0);
+            list.remove(length - 1);
+            length-=2;
+        }
+        return map;
     }
     public static void main(String[] args) {
         ArrayList list = findFactors(66);
