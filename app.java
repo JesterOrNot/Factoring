761e19448231ac04ccaafe76a0699a2c5da95130
Sean Hellum <seanhellum45@gmail.com>
2019-11-05 16:40:46 +0000

    update

--- app.java
+++ app.java
@@ -33,14 +33,18 @@ public class app {
         return map;
     }
 
-    public static ArrayList<Integer> findRightFactorSet(ArrayList<Integer> keys, ArrayList<Integer> values, int aVal,
-            int bVal, int cVal) {
+    public static ArrayList<Integer> findRightFactorSet(int aVal, int bVal, int cVal) {
         ArrayList<Integer> awns = new ArrayList<Integer>();
-        // int length = 0;
-        // for (int var : keys) {
-        // length++;
-        // }
-        // int numOfPairs = length / 2;
+        ArrayList<Integer> factors = findFactors(cVal);
+        HashMap<Integer, Integer> map = assembleTable(factors);
+        ArrayList<Integer> keys = new ArrayList<Integer>();
+        ArrayList<Integer> values = new ArrayList<Integer>();
+        for (int i : map.keySet()) {
+            keys.add(i);
+        }
+        for (int i : map.values()) {
+            values.add(i);
+        }
         if (aVal == 1) {
             for (int i = 0; i <= keys.size() - 1; i++) {
                 if ((keys.get(i) + values.get(i) == bVal) && (keys.get(i) * values.get(i) == cVal)) {
@@ -53,26 +57,7 @@ public class app {
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = findFactors(56);
-        HashMap<Integer, Integer> map = assembleTable(list);
-        ArrayList<Integer> list2 = findFactors(14);
-        HashMap<Integer, Integer> map2 = assembleTable(list2);
-        ArrayList<Integer> keys = new ArrayList<Integer>();
-        ArrayList<Integer> values = new ArrayList<Integer>();
-        for (int i : map.keySet()) {
-            keys.add(i);
-        }
-        for (int i : map.values()) {
-            values.add(i);
-        }
-        for (int i : map2.keySet()) {
-            keys.add(i);
-        }
-        for (int i : map2.values()) {
-            values.add(i);
-        }
-        System.out.println(findRightFactorSet(keys, values, 1, 15, 56));
-        System.out.println(findRightFactorSet(keys, values, 1, 9, 14));
-        System.out.println(values + "\n" + keys);
+        System.out.println(findRightFactorSet(1, 15, 56));
+        System.out.println(findRightFactorSet(1, 9, 14));
     }
 }
