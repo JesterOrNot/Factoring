14cf653a54a2a4c5db076ec3d05cd02ed13dacfb
Sean Hellum <seanhellum45@gmail.com>
2019-11-05 13:46:14 +0000

    update

--- app.java
+++ app.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
  * app
  */
 public class app {
-    static ArrayList<Integer> findFactors(int num) {
+    public static ArrayList<Integer> findFactors(int num) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         for (int i = num; i >= 1; i--) {
             if (num % i == 0) {
@@ -15,7 +15,7 @@ public class app {
         return list;
     }
 
-    static HashMap<Integer, Integer> assembleTable(ArrayList<Integer> list) {
+    public static HashMap<Integer, Integer> assembleTable(ArrayList<Integer> list) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         int length = 0;
         for (int var : list) {
@@ -38,13 +38,13 @@ public class app {
         HashMap<Integer, Integer> map = assembleTable(list);
         ArrayList<Integer> keys = new ArrayList<Integer>();
         ArrayList<Integer> values = new ArrayList<Integer>();
-        System.out.println();
         for (int i : map.keySet()) {
             keys.add(i);
         }
         for (int i : map.values()) {
             values.add(i);
         }
-        System.out.println("");
+        System.out.println(keys);
+        System.out.println(values);
     }
 }
